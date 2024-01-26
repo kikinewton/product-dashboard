ef050ee59089c20ca2b13740225dc18955ed3652
Derrick <derrickagyemang12@gmail.com>
2024-01-26 22:19:04 +0200

    CORS enabled

--- src/main/java/com/bsupply/productdashboard/configuration/WebSecurityConfiguration.java
+++ src/main/java/com/bsupply/productdashboard/configuration/WebSecurityConfiguration.java
@@ -6,6 +6,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 public class WebSecurityConfiguration {
@@ -20,6 +25,22 @@ public class WebSecurityConfiguration {
                 )
                 .cors(Customizer.withDefaults())
                 .build();
+    }
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final var configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("*");
+
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setExposedHeaders(Arrays.asList("*"));
+
+        final var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
 
