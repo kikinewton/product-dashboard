bf80a3638e107ef841c0a652b6eb36cd35f2d864
Derrick <derrickagyemang12@gmail.com>
2024-02-21 20:35:10 +0200

    Add cross origin to auth endpoint

--- src/main/java/com/bsupply/productdashboard/controller/AuthenticationController.java
+++ src/main/java/com/bsupply/productdashboard/controller/AuthenticationController.java
@@ -11,6 +11,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Validated
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthenticationController {
