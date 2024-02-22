725d07697e40688670ece3a9cac99492f043acab
Derrick <derrickagyemang12@gmail.com>
2024-02-22 18:45:33 +0200

    Change userResponse to user

--- src/main/java/com/bsupply/productdashboard/dto/response/LoginResponse.java
+++ src/main/java/com/bsupply/productdashboard/dto/response/LoginResponse.java
@@ -1,4 +1,4 @@
 package com.bsupply.productdashboard.dto.response;
 
-public record LoginResponse(String token, long expiresIn, UserResponse userResponse) {
+public record LoginResponse(String token, long expiresIn, UserResponse user) {
 }
